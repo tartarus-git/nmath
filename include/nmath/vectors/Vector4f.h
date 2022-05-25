d6b5bd416066e6e70c8f8d68782a45f26e9ad2b4
tartarus-git <niktedig@gmail.com>
2022-05-26 01:49:05 +0200

    Botched in support for Vector4f.

--- include/nmath/vectors/Vector4f.h
+++ include/nmath/vectors/Vector4f.h
@@ -1,15 +1,17 @@
 #pragma once
 
-#include "math/Vector3f.h"
+#include "Vector3f.h"
 
-class Vector4f {
-public:
-	float x;
-	float y;
-	float z;
-	float w;
+namespace nmath {
+	class Vector4f {
+	public:
+		float x;
+		float y;
+		float z;
+		float w;
 
-	constexpr Vector4f() = default;
-	constexpr Vector4f(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) { }
-	constexpr Vector4f(Vector3f baseVector, float w) : x(baseVector.x), y(baseVector.y), z(baseVector.z), w(w) { }
-};
+		constexpr Vector4f() = default;
+		constexpr Vector4f(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) { }
+		constexpr Vector4f(nmath::Vector3f baseVector, float w) : x(baseVector.x), y(baseVector.y), z(baseVector.z), w(w) { }
+	};
+}
